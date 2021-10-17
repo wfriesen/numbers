c308430b2cc21f5aec08787e421a942e176fac80
William Friesen <wfriesen@gmail.com>
2021-10-17 19:39:49 +1100

    wrap in main function

--- numbers.py
+++ numbers.py
@@ -5,43 +5,47 @@ target = 242
 perms = []
 
 
-def go(start, perm):
+def build_permutations(start, perm):
     newperm = perm.copy()
     if start > 9:
         perms.append(newperm)
         return
 
     for k in ["*", "/", "+", "-"]:
-        go(start + 2, newperm[:start] + [k] + newperm[start:])
+        build_permutations(start + 2, newperm[:start] + [k] + newperm[start:])
 
 
-for i in permutations(numbers):
-    perm = list(i)
-    go(1, perm)
+def main():
+
+    for i in permutations(numbers):
+        perm = list(i)
+        build_permutations(1, perm)
+
+    def solved(perm):
+        sols = perm.copy()
+        if len(sols) == 1:
+            return sols[0] == target
+
+        left = sols.pop(0)
+        operator = sols.pop(0)
+        right = sols.pop(0)
+
+        if operator == "*":
+            result = left * right
+        elif operator == "/":
+            result = left / right
+        elif operator == "+":
+            result = left + right
+        elif operator == "-":
+            result = left - right
+
+        return solved([result] + sols)
+
+    for perm in perms:
+        s = solved(perm)
+        if solved(perm) is True:
+            print(perm)
 
 
-def solved(perm):
-    sols = perm.copy()
-    if len(sols) == 1:
-        return sols[0] == target
-
-    left = sols.pop(0)
-    operator = sols.pop(0)
-    right = sols.pop(0)
-
-    if operator == "*":
-        result = left * right
-    elif operator == "/":
-        result = left / right
-    elif operator == "+":
-        result = left + right
-    elif operator == "-":
-        result = left - right
-
-    return solved([result] + sols)
-
-
-for perm in perms:
-    s = solved(perm)
-    if solved(perm) is True:
-        print(perm)
+if __name__ == "__main__":
+    main()
